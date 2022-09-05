00ddd928a6dc086e17a077693f3f406d15b6b674
Jeff Song <cass@b01-aruba-authenticated-10-110-203-120.wlan.cpp.edu>
2022-09-05 16:57:56 -0700

    1st Commit

---
+++ AsciiArt.java
@@ -0,0 +1,57 @@
+/***************************************************************  
+*  file: AsciiArt.java  
+*  author: Jeff Song  
+*  class: CS 1400.03 – Programming and Problem Solving  
+*  
+*  assignment: program 1  
+*  date last modified: 2/6/2022  
+*  
+*  purpose: This program prints a cool picture of Charizard.
+*  
+****************************************************************/  
+
+public class AsciiArt{  
+    public static void main(String[] args){  
+        System.out.println("                 .\"-,.__");
+	System.out.println("                 `.     `.  ,");
+	System.out.println("              .--'  .._,'\"-' `.");
+	System.out.println("             .    .'         `'");
+	System.out.println("             `.   /          ,'");
+	System.out.println("               `  '--.   ,-\"'");
+	System.out.println("                `\"`   |  \\");
+	System.out.println("                   -. \\, |");
+	System.out.println("                    `--Y.'      ___.");
+	System.out.println("                         \\     L._, \\");
+	System.out.println("               _.,        `.   <  <\\                _");
+	System.out.println("             ,' '           `, `.   | \\            ( `");
+	System.out.println("          ../, `.            `  |    .\\`.           \\ \\_");
+	System.out.println("         ,' ,..  .           _.,'    ||\\l            )  '\".");
+	System.out.println("        , ,'   \\           ,'.-.`-._,'  |           .  _._`.");
+	System.out.println("      ,' /      \\ \\        `' ' `--/   | \\          / /   ..\\");
+	System.out.println("    .'  /        \\ .         |\\__ - _ ,'` `        / /     `.`.");
+	System.out.println("    |  '          ..         `-...-\"  |  `-'      / /        . `.");
+	System.out.println("    | /           |L__           |    |          / /          `. `.");
+	System.out.println("   , /            .   .          |    |         / /             ` `");
+	System.out.println("  / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \\");
+	System.out.println(" / .           \\\"`_/. `-_ \\_,.  ,'    +-' `-'  _,        ..,-.    \\`.");
+	System.out.println(".  '         .-f    ,'   `    '.       \\__.---'     _   .'   '     \\ \\");
+	System.out.println("' /          `.'    l     .' /          \\..      ,_|/   `.  ,'`     L`");
+	System.out.println("|'      _.-\"\"` `.    \\ _,'  `            \\ `.___`.'\"`-.  , |   |    | \\");
+	System.out.println("||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|");
+	System.out.println("||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||");
+	System.out.println("|| '              V      / /           `   | `   ,'   ,' '.    !  `. ||");
+	System.out.println("||/            _,-------7 '              . |  `-'    l         /    `||");
+	System.out.println(". |          ,' .-   ,' ||               | .-.        `.      .'     ||");
+	System.out.println(" `'        ,'    `\".'    |               |    `.        '. -.'       `'");
+	System.out.println("          /      ,'      |               |,'    \\-.._,.'/'");
+	System.out.println("          .     /        .               .       \\    .''");
+	System.out.println("        .`.    |         `.             /         :_,'.'");
+	System.out.println("          \\ `...\\   _     ,'-.        .'         /_.-'");
+	System.out.println("           `-.__ `,  `'   .  _.>----''.  _  __  /");
+	System.out.println("                .'        /\"'          |  \"'   '_");
+	System.out.println("               /_|.-'\\ ,\".             '.'`__'-( \\");
+	System.out.println("                 / ,\"'\"\\,'               `/  `-.|\"");
+	System.out.println("                           I Choose You!");
+
+    }  
+}  
